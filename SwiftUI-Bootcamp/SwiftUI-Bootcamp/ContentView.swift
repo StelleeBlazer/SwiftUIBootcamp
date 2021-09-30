261fa43df303d8a6be3f37b4c6b8e7908c2a7c04
Rahmananda <55073796+rahmanandas@users.noreply.github.com>
2021-09-30 08:38:06 +0700

    Add files via upload

---
+++ SwiftUI-Bootcamp/SwiftUI-Bootcamp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swiftui Boothcamp Idn
+//
+//  Created by Harsyah on 27/09/21.
+//  Copyright © 2021 Harsyah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
